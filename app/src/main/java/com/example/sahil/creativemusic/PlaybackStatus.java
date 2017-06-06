792acb2b10dd60713a5f5207f3c63b13ba389906
SAHIL <dharmvir.nitdgp@gmail.com>
2017-06-06 12:53:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sahil/creativemusic/PlaybackStatus.java
@@ -0,0 +1,11 @@
+package com.example.sahil.creativemusic;
+
+/**
+ * Created by SAHIL on 5/27/2017.
+ */
+
+public enum PlaybackStatus {
+
+    PLAYING,
+    PAUSED
+}
---
+++ app/src/main/java/com/example/sahil/creativemusic/onItemClickListener.java
@@ -0,0 +1,12 @@
+package com.example.sahil.creativemusic;
+
+import android.view.View;
+
+/**
+ * Created by SAHIL on 5/27/2017.
+ */
+
+public interface onItemClickListener {
+
+    public void onClick(View view, int index);
+}
